44420fd306cf5eba86c4188beeb1846d80241cfb
Ginger <ggmmallow@gmail.com>
2026-04-21 15:52:20 +0700

    first commit

---
+++ Color_p212.cs
@@ -0,0 +1,32 @@
+// class ColorProgram{
+//     static void Main(){
+//         Color White = new Color(255, 255, 255);
+//         Color Black = new Color(0,0,0);
+//         Color Red = new Color(255, 0,0);
+//         Color Orange = new Color(255, 165, 0);
+//         Color Yellow = new Color(255, 255, 0);
+//         Color Green = new Color(0, 128, 0);
+//         Color Blue = new Color(0, 0, 255);
+//         Color Purple = new Color(128, 0, 128);
+
+//         Color RandomColor = Color.CreateColor(0,2,3);
+//         Console.WriteLine($"{White.red}, {White.green}, {White.blue}");
+//         Console.WriteLine($"{RandomColor.red}, {RandomColor.green}, {RandomColor.blue}");
+
+//     }
+// }
+
+public class Color{
+    public int red;
+    public int green;
+    public int blue;
+
+    public Color(int r, int g, int b){
+        red = r;
+        green = g;
+        blue = b;
+    }
+    public static Color CreateColor(int r, int g, int b){
+        return new Color(r, g, b);
+    }
+}
---
+++ p156_Simula.cs
@@ -0,0 +1,33 @@
+using System;
+
+
+class Program {
+    public ChestState currentState = ChestState.locked;
+
+    static void Main(){
+        Program p = new Program();
+        p.Run();
+    }
+    void Run(){
+        while (true){
+            Console.WriteLine($"The chest is {currentState}. What do you want to do? ");
+            string action = Console.ReadLine()?.ToLower() ?? "";
+        
+            if (action == "unlock" && currentState == ChestState.locked)
+                currentState = ChestState.closed;
+
+            else if (action == "open" && currentState == ChestState.closed)
+                currentState = ChestState.open;
+
+            else if (action == "close" && currentState == ChestState.open)
+                currentState = ChestState.closed;        
+        
+            else {
+                Console.WriteLine("Invalid Action.");
+            }
+        }
+    }
+
+    public enum ChestState {open, closed, locked}
+}
+
